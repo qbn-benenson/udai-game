5ba6ad043bbef70526b7cbbf5944be0e941d542b
Samuel Benenson <sam.benenson@gmail.com>
2021-04-20 13:12:53 -0400

    adding some 2D packages, along with player movement

---
+++ Udai/Assets/Script/PlayerController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D rb;
+
+    private Animator animator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 direction = Vector2.zero;
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction.x = -1f;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            direction.x = 1f;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction.y = 1f;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            direction.y = -1f;
+        }
+
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            speed = 2f;
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            speed = 5f;
+        }
+        direction.Normalize();
+        rb.velocity = speed * direction;
+    }
+}
